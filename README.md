41f0a65c410bd51d9768330387badcbda5fd3701
rizwanadev <rizwanadev20@gmail.com>
2023-02-09 14:14:19 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DemoPackageGit
+
+A description of this package.
---
+++ Sources/DemoPackageGit/DemoPackageGit.swift
@@ -0,0 +1,6 @@
+public struct DemoPackageGit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
