c09ef8f8476733ba533ede423030cbede7b9afdd
rizwanadev <rizwanadev20@gmail.com>
2023-02-09 14:23:41 +0530

    created demo

--- Sources/DemoPackageGit/DemoPackageGit.swift
+++ Sources/DemoPackageGit/DemoPackageGit.swift
@@ -1,6 +1,12 @@
 public struct DemoPackageGit {
+    
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    
+    public func demoGitPackage(){
+        print("Prinitng from \(#function)")
     }
 }
